238cca11bdbdb2c617ca97190388a0777f066b96
Tsutajiro <y.sugie.15739d@gmail.com>
2020-07-20 06:02:14 +0900

    [WIP] walking around

--- tsutaj/solution/solver.cpp
+++ tsutaj/solution/solver.cpp
@@ -1,4 +1,5 @@
 #include "galaxy.hpp"
+#include <cmath>
 
 using Vec = galaxy::Vec;
 
@@ -31,6 +32,38 @@ bool universe_check(const Vec& p0, const Vec& d0, int n, const galaxy::StaticGam
 		d = next.second;
 	}
 	return true;
+}
+
+Vec calc_ideal_velocity(double theta) {
+    const double pi = acos(-1);
+    // 第 3 象限 [-pi, -pi/2)
+    if(theta < -pi/2) {
+        // -pi   -> (0, -7)
+        // -pi/2 -> (7, 0)
+        int stage = (int)((-pi/2 - theta) / 7);
+        return Vec(0+stage, -7+stage);
+    }
+    // 第 4 象限 [-pi/2, 0)
+    else if(theta < 0) {
+        // -pi/2 -> (7, 0)
+        // 0     -> (0, 7)
+        int stage = (int)((0 - theta) / 7);
+        return Vec(7-stage, 0+stage);
+    }
+    // 第 1 象限 [0, pi/2)
+    else if(theta < pi/2) {
+        // 0     -> (0, 7)
+        // pi/2  -> (-7, 0)
+        int stage = (int)((pi/2 - theta) / 7);
+        return Vec(0-stage, 7-stage);
+    }
+    // 第 2 象限 [pi/2, pi)
+    else {
+        // pi/2  -> (-7, 0)
+        // pi    -> (0, -7)
+        int stage = (int)((pi - theta) / 7);
+        return Vec(-7+stage, 0-stage);
+    }
 }
 
 int main(int argc, char *argv[]){
@@ -70,21 +103,35 @@ int main(int argc, char *argv[]){
                 if(ship.role != res.static_info.self_role) { continue; }
                 const Vec p = ship.pos, d = ship.vel;
 
-                int pw = p.x*p.x + p.y*p.y;
-                if(std::abs(pw - 5000) > 100) {
-                    int best_dx = -1, best_dy = -1, best_diff = 1000000;
+                if(std::abs(d.x) + std::abs(d.y) >= 7) {
+                    double theta = atan2(p.y, p.x);
+                    const Vec ideal_d = calc_ideal_velocity(theta);
+                    int dx = 0, dy = 0;
+                    if(d.x < ideal_d.x) dx++;
+                    if(d.x > ideal_d.x) dx--;
+                    if(d.y < ideal_d.y) dy++;
+                    if(d.y > ideal_d.y) dy--;
+                    cmds.accel(ship.id, Vec(dx, dy));
+                }
+                else {
+                    int best_dx = -1, best_dy = -1, best_diff = 100000000;
                     for(int dx=-1; dx<=1; dx++) {
                         for(int dy=-1; dy<=1; dy++) {
-                            const auto next = simulate(p, Vec(d.x - dx, d.y - dy));
-                            const Vec np = next.first;
-                            int npw = np.x*np.x + np.y*np.y;
-                            int diff = std::abs(npw - 5000);
+                            if(std::abs(d.x - dx) + std::abs(d.y - dy)
+                               <= std::abs(d.x) + std::abs(d.y)) {
+                                continue;
+                            }
+                            const auto next = simulate(p, Vec(d.x - dx, d.y - dy)).first;
+                            int diff = next.x*next.x + next.y*next.y - 5000;
+                            if(diff < 0) continue;
                             if(best_diff > diff) {
                                 best_diff = diff;
-                                best_dx = dx, best_dy = dy;
+                                best_dx = dx;
+                                best_dy = dy;
                             }
                         }
                     }
+                    fprintf(stderr, "best_dx = %d, best_dy = %d, best_diff = %d\n", best_dx, best_dy, best_diff);
                     cmds.accel(ship.id, Vec(best_dx, best_dy));
                 }
             }
