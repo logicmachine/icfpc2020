154e66b530a96b43d38649f7071ccdc7cf3a8d71
Takaaki Hiragushi <ent@arrize.net>
2020-07-20 05:34:45 +0900

    History-based attacking

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd libgalaxy/example
-mkdir ../../build
-g++ -std=c++14 -I../include -lcurl -o ../../build/main accel.cpp
+cd logicmachine
+mkdir ../build
+g++ -std=c++14 -I../libgalaxy/include -lcurl -o ../build/main main.cpp
