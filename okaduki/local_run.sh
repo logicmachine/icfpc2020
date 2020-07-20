becdd3523c6f6dfa44e9d99b4ff40224f4a86016
okaduki <okaduki1@gmail.com>
2020-07-20 16:56:56 +0900

    Implement defender orbit (insufficient)

--- okaduki/local_run.sh
+++ okaduki/local_run.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+if [[ $# < 2 ]]; then
+  echo "usage: $0 attacker.out defender.out"
+  exit 0
+fi
+
+atk="$1"
+def="$2"
+
 pushd ../libgalaxy/example
 keys=$(./room_create.sh)
 popd
@@ -13,6 +21,7 @@ def_key=$(echo $keys | cut -d' ' -f4)
 
 uri='https://icfpc2020-api.testkontur.ru/aliens/send?apiKey=b0a3d915b8d742a39897ab4dab931721'
 
-../build/main.old $uri $atk_key > atk.log 2>&1 &
-../build/main "$uri" $def_key > def.log 2>&1 &
+trap 'kill $(jobs -p)' SIGINT
+$atk $uri $atk_key > atk.log 2>&1 &
+$def $uri $def_key > def.log 2>&1 &
 wait
--- okaduki/solver.cpp
+++ okaduki/solver.cpp
@@ -17,6 +17,17 @@ const double RELATIVE_ANGLE_THRESHOLD = 0.2;
 //----------------------------------------------------------------------------
 using Vec = galaxy::Vec;
 
+uint32_t xor64(void) {
+  static uint64_t x = 88172645463325252ULL;
+  x = x ^ (x << 13); x = x ^ (x >> 7);
+  return x = x ^ (x << 17);
+}
+
+int32_t random(int32_t a, int32_t b) {
+	uint32_t x = xor64() % (b - a);
+	return x + a;
+}
+
 Vec gravity(const Vec p){
 	const long abs_x = std::abs(p.x), abs_y = std::abs(p.y);
 	if(abs_x > abs_y){
@@ -35,26 +46,130 @@ std::pair<Vec, Vec> simulate(const Vec& p0, const Vec& d0){
 	return std::make_pair(p, d);
 }
 
-bool universe_check(const Vec& p0, const Vec& d0, int n, long r){
+void dump_vec(const Vec& vec, std::ostream& os) {
+	os << "(" << vec.x << ", " << vec.y << ")" << std::endl;
+}
+
+bool universe_check(const Vec& p0, const Vec& d0, int n, long fld_r, long planet_r = -1){
 	Vec p = p0, d = d0;
 	for(int i = 0; i < n; ++i){
 		const auto next = simulate(p, d);
-		if(std::abs(next.first.x) > r){ return false; }
-		if(std::abs(next.first.y) > r){ return false; }
+		if(std::abs(next.first.x) > fld_r){ return false; }
+		if(std::abs(next.first.y) > fld_r){ return false; }
+
+		if(std::abs(next.first.x) <= planet_r
+			&& std::abs(next.first.y) <= planet_r){ return false; }
 		p = next.first;
 		d = next.second;
 	}
 	return true;
 }
 
-Vec compute_accel(const Vec& p, const Vec& d, long radius){
+int universe_check_step(const Vec& p0, const Vec& d0, long fld_r, long planet_r = -1){
+	const int MAX = 256;
+	Vec p = p0, d = d0;
+	for(int i = 0; i < MAX; ++i){
+		const auto next = simulate(p, d);
+		if(std::abs(next.first.x) > fld_r){ return i; }
+		if(std::abs(next.first.y) > fld_r){ return i; }
+
+		if(std::abs(next.first.x) <= planet_r
+			&& std::abs(next.first.y) <= planet_r){ return i; }
+		p = next.first;
+		d = next.second;
+	}
+	return MAX;
+}
+
+Vec compute_accel(const Vec& p, const Vec& d, long fld_radius){
 	const Vec dd(p.x >= 0 ? -1 : 1, p.y >= 0 ? -1 : 1);
 	const auto next = simulate(p, Vec(d.x - dd.x, d.y - dd.y));
-	if(universe_check(next.first, next.second, UNIVERSE_CHECK_ITERATIONS, radius)){
+	if(universe_check(next.first, next.second, UNIVERSE_CHECK_ITERATIONS, fld_radius, -1)){
 		return dd;
 	}else{
 		return Vec();
 	}
+}
+
+Vec compute_accel_far_away(const Vec& p, const Vec& d, long fld_radius){
+	const Vec dd(p.x >= 0 ? -1 : 1, p.y >= 0 ? -1 : 1);
+	const auto next = simulate(p, Vec(d.x - dd.x, d.y - dd.y));
+	if(universe_check(next.first, next.second, UNIVERSE_CHECK_ITERATIONS, fld_radius, -1)){
+		return dd;
+	}else{
+		return Vec();
+	}
+}
+
+Vec random_accel(const Vec& p, const Vec& d, long fld_radius, long planet_radius){
+	const long abs_x = std::abs(p.x), abs_y = std::abs(p.y);
+	Vec dd;
+	if (abs_x < abs_y) {
+		dd.y = random(0, 2) * 2 - 1;
+		dd.x = 0;
+	}
+	else {
+		dd.x = random(0, 2) * 2 - 1;
+		dd.y = 0;
+	}
+
+	const auto next = simulate(p, Vec(d.x - dd.x, d.y - dd.y));
+	if(universe_check(next.first, next.second, UNIVERSE_CHECK_ITERATIONS, fld_radius, planet_radius)){
+		return dd;
+	}else{
+		return Vec();
+	}
+}
+
+// pos, vel
+using Cache = std::tuple<long,long,long,long>;
+void precalc_orbit(long fld_r, long pln_r, std::map<Cache,long>& res) {
+	const long VEL_SIZE = 10;
+
+	for (long y = -fld_r; y <= fld_r; ++y) {
+		for (long x = -fld_r; x <= fld_r; ++x) {
+			for (long vy = -VEL_SIZE; vy <= VEL_SIZE; ++vy) {
+				for (long vx = -VEL_SIZE; vx <= VEL_SIZE; ++vx) {
+					Vec pos(x, y);
+					Vec vel(vx, vy);
+					
+					auto idx = std::make_tuple(x, y, vx, vy);
+					res[idx] = universe_check_step(pos, vel, fld_r, pln_r);
+				}
+			}
+		}
+	}
+}
+
+long get_orbit_step(const std::map<Cache,long>& cache, const Vec& pos, const Vec& vel) {
+	auto idx = std::make_tuple(pos.x, pos.y, vel.x, vel.y);
+	auto it = cache.find(idx);
+
+	if (it != std::end(cache))
+		return it->second;
+	return -1;
+}
+
+bool find_neighbor_orbit(
+	const std::map<Cache,long>& memo
+	, const Vec& pos
+	, const Vec& vel
+	, long time
+	, Vec& res
+) {
+	for (long vy = -1; vy <= 1; ++vy) {
+		for (long vx = -1; vx <= 1; ++vx) {
+			const auto next = simulate(pos, Vec(vel.x - vx, vel.y - vy));
+			long step = get_orbit_step(memo, next.first, next.second);
+
+			if (step >= time) {
+				res.x = vx;
+				res.y = vy;
+				return true;
+			}
+		}	
+	}
+	return false;
 }
 
 //----------------------------------------------------------------------------
@@ -78,6 +193,10 @@ void defender(
 		-  2 * ship_params.x3;
 	res = ctx.start(ship_params);
 
+	std::map<Cache,long> memo;
+	precalc_orbit(res.static_info.universe_radius, res.static_info.galaxy_radius, memo);
+	std::map<long, bool> on_orbit;
+
 	// Command loop
 	while(res.stage == galaxy::GameStage::RUNNING){
 		res.dump(std::cerr);
@@ -85,8 +204,21 @@ void defender(
 		for(const auto& sac : res.state.ships){
 			const auto& ship = sac.ship;
 			if(ship.role != res.static_info.self_role){ continue; }
-			const auto accel = compute_accel(
-				ship.pos, ship.vel, res.static_info.universe_radius);
+
+			if (!on_orbit.count(ship.id)) on_orbit[ship.id] = false;
+			if (on_orbit[ship.id]) continue;
+
+			long rest_time = res.static_info.time_limit - res.state.elapsed + 1;
+			Vec accel;
+			bool found = find_neighbor_orbit(memo, ship.pos, ship.vel, rest_time, accel);
+			if (!found) {
+				accel = compute_accel_far_away(
+					ship.pos, ship.vel, res.static_info.universe_radius);
+			}
+			else {
+				on_orbit[ship.id] = true;
+			}
+
 			if(accel.x != 0 || accel.y != 0){
 				cmds.accel(ship.id, accel);
 			}
